b34e3b67bd42b01240c6ed694189a49b7a0a5ad2
Eric Wen <eericwen@icloud.com>
2019-04-21 17:23:44 +0800

    [+] Add solution to lab 1.1

--- lab1/exercise1.ml
+++ lab1/exercise1.ml
@@ -4,16 +4,30 @@ exception Unimplemented
 
 let main () =
   let rec gcd (m : int) (n : int) : int =
-       raise Unimplemented
+    match n with
+    | 0 -> m
+    | _ -> gcd n (m mod n)
   in
 
   assert (gcd 5 2 = 1);
   assert (gcd 10 2 = 2);
   assert (gcd 48 18 = 6);
 
-  let rec fizz_buzz (n : int) : unit =
-    raise Unimplemented
+  let fizz_buzz (n : int) : unit =
+    let rec fizz_buzz_iter (i : int) : unit =
+      if i < n
+      then
+        ((match (i mod 3 = 0, i mod 5 = 0) with
+        | (true, true) -> Printf.printf "fizzbuzz\n"
+        | (true, false) -> Printf.printf "fizz\n"
+        | (false, true) -> Printf.printf "buzz\n"
+        | _ -> ());
+        fizz_buzz_iter (i + 1))
+      else ()
+    in fizz_buzz_iter 0
   in
+
+  fizz_buzz 20;
 
   let read_line () : string =
     match In_channel.input_line In_channel.stdin with
@@ -22,11 +36,21 @@ let main () =
   in
 
   let rec read_password (password : string) : unit =
-    raise Unimplemented
+    if read_line () = password
+    then ()
+    else read_password password
   in
 
   let substring_match (pattern : string) (source : string) : int option =
-    raise Unimplemented
+    let pattern_length = String.length pattern in
+    let source_length = String.length source in
+    let rec match_iter (i : int) : int option =
+      if (i + pattern_length) > source_length
+      then None
+      else if pattern = String.slice source i (i + pattern_length)
+      then Some i
+      else match_iter (i + 1)
+    in match_iter 0
   in
 
   assert (substring_match "foo" "foobar" = Some 0);
