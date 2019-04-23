3d6bc4126b84bfd163bf0bf7fbf459910c235110
Eric Wen <eericwen@icloud.com>
2019-04-23 20:25:03 +0800

    [+] Add solution to assignment 2.1

--- assign2/program/src/ast.ml
+++ assign2/program/src/ast.ml
@@ -40,15 +40,24 @@ module Expr = struct
    * implementation. *)
   let rec substitute (x : string) (e' : t) (e : t) : t =
     match e with
-    | Int n -> raise Unimplemented
-    | Var x' -> raise Unimplemented
-    | Binop (b, t1, t2) -> raise Unimplemented
-    | Lam (x', tau, body) -> raise Unimplemented
-    | App (t1, t2) -> raise Unimplemented
-    | Pair (e1, e2) -> raise Unimplemented
-    | Project (e, d) -> raise Unimplemented
-    | Inject (e, d, tau) -> raise Unimplemented
-    | Case (e, (x1, e1), (x2, e2)) -> raise Unimplemented
+    | Int n -> e
+    | Var x' ->
+      (if x' = x
+       then e'
+       else e)
+    | Binop (b, t1, t2) -> Binop (b, substitute x e' t1, substitute x e' t2)
+    | Lam (x', tau, body) ->
+      (if x' = x
+       then e
+       else Lam (x', tau, substitute x e' body))
+    | App (t1, t2) -> App (substitute x e' t1, substitute x e' t2)
+    | Pair (e1, e2) -> Pair (substitute x e' e1, substitute x e' e2)
+    | Project (e, d) -> Project (substitute x e' e, d)
+    | Inject (e, d, tau) -> Inject (substitute x e' e, d, tau)
+    | Case (e, (x1, e1), (x2, e2)) ->
+      Case (substitute x e' e,
+            (x1, (if x = x1 then e1 else substitute x e' e1)),
+            (x2, (if x = x2 then e2 else substitute x e' e2)))
 
   let inline_tests () =
     let t1 = App(Lam("x", Type.Int, Var "x"), Var "y") in
@@ -63,8 +72,7 @@ module Expr = struct
             Binop(Add, Var "x", Lam("x", Type.Int, Int 0)))
 
   (* Uncomment the line below when you want to run the inline tests. *)
-  (* let () = inline_tests () *)
-
+  let () = inline_tests ()
 
   let to_string e = Sexp.to_string_hum (sexp_of_t e)
 end
